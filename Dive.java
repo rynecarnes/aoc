7d281c3e199f211b7bd61a0a2a6bc978b62dd19c
rynecarnes <ryne.carnes@gmail.com>
2021-12-11 12:34:59 -0500

    Adding Day 2 Part 1 files

---
+++ Dive.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Dive{ 
+    
+    public static void main(String[] args) {
+        try {
+            File file = new File("day2Input.txt");
+            Scanner scanner = new Scanner(file);
+            long x = 0;
+            long y = 0;
+            
+
+            while (scanner.hasNextLine()) {
+                String[] command = scanner.nextLine().split(" ");
+                if ("forward".equals(command[0])) {
+                    x += Long.parseLong(command[1]);    
+                }
+                else if ("down".equals(command[0])) {
+                    y += Long.parseLong(command[1]);
+                }
+                else if ("up".equals(command[0])) {
+                    y -= Long.parseLong(command[1]);
+                }
+                System.out.println("New Position: " + x + ", " + y);
+            }
+            System.out.println(x * y);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File could not be found.");
+            e.printStackTrace();
+        }
+    }
+
+}
